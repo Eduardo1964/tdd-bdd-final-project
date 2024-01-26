9532fe6c0554fc48672f1a0390752218dc341404
Eduardo1964 <eduardo.sereguin@gmail.com>
2024-01-26 14:03:05 -0500

    exercise

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -33,17 +33,30 @@ from selenium.webdriver.support import expected_conditions
 ID_PREFIX = 'product_'
 
 
-@when('I visit the "Home Page"')
-def step_impl(context):
-    """ Make a call to the base URL """
-    context.driver.get(context.base_url)
-    # Uncomment next line to take a screenshot of the web page
-    # context.driver.save_screenshot('home_page.png')
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
 
 @then('I should see "{message}" in the title')
 def step_impl(context, message):
     """ Check the document title for a message """
     assert(message in context.driver.title)
+
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element_by_id('search_results')
+    assert(name not in element.text)
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert(found)
 
 @then('I should not see "{text_string}"')
 def step_impl(context, text_string):
